cb2c99f4a0545b176f627526231fbd1108328b51
ahmed algrni <ahmedalg321@gmail.com>
2019-01-31 22:55:36 +0300

    last

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,9 @@
-/*import 'package:flutter/material.dart';
-import 'login.dart';
-import 'home_widget.dart';
-import 'event_info_page.dart';
-import 'Record_Page.dart';
-import 'package:firebase_database/firebase_database.dart';
 
-
-void main() => runApp(App());
-
-class App extends StatelessWidget {
-  final routes = <String, WidgetBuilder>{
-    LoginPage.tag: (context) => LoginPage(),
-    Home.tag: (context) =>  Home(),
-    EventInfoPage.tag: (context) => EventInfoPage(),
-    RecordPage.tag: (context) => RecordPage(),
-  };
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Flutter App',
-      home: LoginPage(),
-      routes: routes,
-    );
-  }
-}*/
 
 
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-//import 'ShowDataPage.dart';
+
+
 import 'login.dart';
 import 'home_widget.dart';
 import 'event_info_page.dart';
